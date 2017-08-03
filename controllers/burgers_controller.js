e88f2d2109653f15278f6b16992234ddcd0cc51d
brianpatrickhummel <frankgucci@mac.com>
2017-08-02 23:02:04 -0500

    Customer associations are complete

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,49 +1,49 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  app.get("/", function(req, res){
-    db.Burgers.findAll().then(function(result){
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    db.Burgers.findAll({
+      include: db.Customers
+    }).then(function (result) {
+      console.log(result);
       var burgersObj = {
-      burgers: result
-    };
-    res.render("index", burgersObj);
+        burgers: result
+      };
+      res.render("index", burgersObj);
     });
   });
 
-  app.post("/", function(req,res){
+  app.post("/", function (req, res) {
     db.Burgers.create({
       burger_name: req.body.burger_name
-    }).then(function(){
+    }).then(function () {
       res.redirect('/');
-    });      
+    });
   });
 
-  app.put("/", function(req, res){
+  app.put("/devour", function (req, res) {
     var customerId;
+    // Check to see if the customer name entered already exists, if not create
     db.Customers.findOrCreate({
       where: {
         customer_name: req.body.customer_name
       }
-    })
-    .spread((user,created) => {
+    })// Take the the returned user data and update/associate the burger record accordingly
+    .spread((user, created) => {
       if (created) console.log("User created");
       customerId = user.id;
-      console.log(customerId);
-      console.log(req.body.devoured);
-      console.log(req.body.id);
       var update = {
-        "devoured" : req.body.devoured,
-        "CustomerId" : customerId
+        "devoured": req.body.devoured,
+        "CustomerId": customerId
       };
       db.Burgers.update(update, {
         where: {
           id: req.body.id
         }
-      }).then(function(){
-        // res.redirect('/');
-      });
-    })
-    
+      })
+    }).then(function () {
+      // res.redirect('/');    EEEEERRRRRRrRRRRRROOOOOORRRRRR
+    });
   });
 };
 
--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,22 @@
-module.exports = function(sequelize, DataTypes){
+module.exports = function (sequelize, DataTypes) {
   var Burgers = sequelize.define("Burgers", {
     burger_name: {
       type: DataTypes.STRING,
       unique: true,
       allowNull: false,
       validation: {
-        lens: [1,55]
-      }
+        lens: [1, 55]
+      },
+      set(val) {
+      this.setDataValue('burger_name', val.toUpperCase());
+    }
     },
     devoured: {
       type: DataTypes.BOOLEAN,
       defaultValue: 0
     }
   });
-  Burgers.associate = function(db) {
+  Burgers.associate = function (db) {
     db.Burgers.belongsTo(db.Customers, {
       foreignKey: {
         allowNull: true

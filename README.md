1bc47841d3a853fd2da4cf9fabe56c28f5167272
brianpatrickhummel <frankgucci@mac.com>
2017-08-03 22:34:14 -0500

    final changes - updated README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# sequelizedBurger
+# Sequelized BurgerTracker
+
+An exercise to demonstrate the use of an Express-based Node.js which will allow client-side manipulation of table data and table associations within a MySQL database via a Sequelize/Sequelize-CLI ORM.
+
+## Back-End
+Node.js-Express Web Server which utilizes Sequelize to interface with a MySQL database. Built with a MVC design pattern.  Views are templated using Handlebars.   Model geneneration and associations via Sequelize CLI.
+
+## Front-End 
+Custom CSS.  Uses Vex jQuery library for Modal dialogue boxes and 'CSSShake' CSS animations.  Responsive-layout.
+
+* Deployed to Heroku using JawsDB for data.
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,13 @@
 var db = require("../models");
 
 module.exports = function (app) {
+
+  // GET request to display all burgers from DATABASE on home page
   app.get("/", function (req, res) {
     db.Burgers.findAll({
-      order: ['updatedAt'],      // Will order the devoured results by time/date devoured
+      // ORDER the devoured results by time/date devoured
+      order: ['updatedAt'],
+      // JOIN to include associated customer if burger has been devoured    
       include: db.Customers
     }).then(function (result) {
       console.log(result);
@@ -14,6 +18,7 @@ module.exports = function (app) {
     });
   });
 
+  // POST/CREATE new burgers in DATABASE
   app.post("/", function (req, res) {
     db.Burgers.create({
       burger_name: req.body.burger_name
@@ -22,6 +27,7 @@ module.exports = function (app) {
     });
   });
 
+  // PUT UPDATE devoured boolean in DATABASE and Find/Create customer to associate
   app.put("/devour", function (req, res) {
     var customerId;
     // Check to see if the customer name entered already exists, if not create
@@ -30,21 +36,21 @@ module.exports = function (app) {
         customer_name: req.body.customer_name
       }
     })// Take the the returned user data and update/associate the burger record accordingly
-      .spread((user, created) => {
-        if (created) console.log("User created");
-        customerId = user.id;
-        var update = {
-          "devoured": req.body.devoured,
-          "CustomerId": customerId
-        };
-        db.Burgers.update(update, {
-          where: {
-            id: req.body.id
-          }
-        }).then(function () {
-          res.redirect(303, '/');
-        });
-      })
+    .spread((user, created) => {
+      if (created) console.log("User created");
+      customerId = user.id;
+      var update = {
+        "devoured": req.body.devoured,
+        "CustomerId": customerId
+      };
+      db.Burgers.update(update, {
+        where: {
+          id: req.body.id
+        }
+      }).then(function () {
+        res.redirect(303, '/');
+      });
+    })
   });
 };
 
